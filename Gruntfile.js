95139ccde3e1b810b46101f7efaa0de4ef89af50
JuanMa Ruiz <ruizjuanma@gmail.com>
2014-11-25 12:24:33 +0100

    Initial commit with contributors and README

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+          base: 'public/'
+        }
+      }
+    },
+		less: {
+			development: {
+				options: {
+					paths: ["./css"],
+					compress: false
+				},
+			files: {
+				"./public/css/style.css": "./less/style.less"
+			}
+		},
+	},
+    watch: {
+      project: {
+        files: ['public/**/*.js', 'public/**/*.html', 'public/**/*.json','public/**/*.css', './less/*.less'],
+        tasks: ["less"],
+        options: {
+          livereload: true
+        },
+        
+      },
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['connect', 'less', 'watch']);
+
+};
