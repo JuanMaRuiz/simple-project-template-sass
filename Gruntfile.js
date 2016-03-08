895b0622a738fd52acc520338a31f7a45e471292
JuanMa Ruiz <ruizjuanma@gmail.com>
2016-03-08 01:23:36 +0100

    Updated Gruntfile with jit-grunt and refactored some tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,62 +1,55 @@
-module.exports = function (grunt) {
+(function(){
+  'use strict';
 
-  grunt.initConfig({
-    connect: {
-      server: {
-        options: {
-          port: 9000,
-          base: 'public/'
+
+  module.exports = function (grunt) {
+
+  require('jit-grunt')(grunt);
+
+    grunt.initConfig({
+      connect: {
+        server: {
+          options: {
+            port: 9000,
+            base: 'public/',
+            open: true,
+            hostname: 'localhost',
+            livereload: 35729
+          }
         }
+      },
+      sass: {
+        dist: {
+          files: {
+            'public/css/style.css': 'scss/style.scss'
+          }
+        }
+    },
+    jshint: {
+      options: {
+          reporter: require('jshint-stylish'),
+          jshintrc : '.jshintrc'
+      },
+      all: {
+        src: [
+          'Gruntfile.js',
+          'app/js/{,*/}*.js'
+        ]
       }
     },
-    sass: {
-      dist: {
-        files: {
-          'public/css/style.css': 'scss/style.scss'
-        }
-      }
-  },
-  jshint: {
-    options: {
-        "bitwise": true,
-        "browser": true,
-        "curly": true,
-        "eqeqeq": true,
-        "eqnull": true,
-        "esnext": true,
-        "immed": true,
-        "jquery": true,
-        "latedef": true,
-        "newcap": true,
-        "noarg": true,
-        "node": true,
-        "strict": false,
-        "trailing": true,
-        "undef": true,
-        "globals": {
-          "jQuery": true,
-          "alert": true
-        }
-      },
-    all: ['Gruntfile.js', 'public/**/*.js']
-  },
-    watch: {
-      project: {
-        files: ['public/**/*.js', 'public/**/*.html', 'public/**/*.json','public/**/*.css', './scss/*.scss'],
-        tasks: ["sass"],
-        options: {
-          livereload: true
+      watch: {
+        project: {
+          files: ['public/**/*.js', 'public/**/*.html', 'public/**/*.json','public/**/*.css', './scss/*.scss'],
+          tasks: ["sass"],
+          options: {
+            livereload: true
+          },
+          
         },
-        
-      },
-    }
-  });
+      }
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['connect', 'sass', 'jshint','watch']);
 
-  grunt.registerTask('default', ['connect', 'sass', 'jshint','watch']);
-
-};
+  };
+})();
